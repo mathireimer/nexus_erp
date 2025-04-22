834d122115fa7f23616c85a8f787b0153f29f3ef
Buffalo Creations <buffalo.creations.official@gmail.com>
2025-04-22 10:48:17 -0400

    Update app.py

--- app.py
+++ app.py
@@ -807,6 +807,5 @@ def get_exchange_rates():
         return jsonify({'error': 'Failed to fetch exchange rates'}), 500
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()  # Create database tables
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
