ca9d095b930cd82c263b7571abdee5fc56dea265
Buffalo Creations <buffalo.creations.official@gmail.com>
2025-04-22 10:53:10 -0400

    Update app.py

--- app.py
+++ app.py
@@ -808,4 +808,5 @@ def get_exchange_rates():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=port)
+
